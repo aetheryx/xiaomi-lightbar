0c247e3b84a12858c3371f16cf3446c733bb22ca
aetheryx <mhzy.ali@gmail.com>
2023-08-05 20:06:16 +0200

    configure MCU

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+u32 counter = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(MONITOR_SPEED);
+  printf("\n\nstart\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  printf("%d\n", counter++);
+  delay(100);
 }
